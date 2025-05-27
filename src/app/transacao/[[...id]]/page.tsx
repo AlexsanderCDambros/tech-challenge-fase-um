199ea1e42fa34e1225af34aee8b34a48a0f4de62
Alexsander Chagas Dambros <alexsanderdambros@gmail.com>
2025-05-27 19:26:48 -0300

    chore: inicio da estilizacao dos inputs

---
+++ src/app/transacao/[[...id]]/page.tsx
@@ -0,0 +1,35 @@
+import Cartao from "@/components/cartao/cartao";
+import FormTransacoes from "@/components/form-transacoes/form-transacoes";
+import { ITransacao } from "@/interfaces/itransacao";
+import { useState } from "react";
+
+async function getTransacao(id: number): Promise<ITransacao | undefined> {
+  try {
+    const res = await fetch('http://127.0.0.1:3001/transacoes/' + id);
+    if (!res.ok) {
+        return undefined; 
+    }
+    return res.json()
+  } catch (error) {
+    return undefined; 
+  }  
+}
+
+export default async function Transacoes({ params }: { params: { id?: string[] } }) {
+    const { id: idArray } = await params;
+    const id = idArray?.[0];
+    let transacao: ITransacao | undefined = undefined;
+
+    if (id) {
+        transacao = await getTransacao(Number(id));
+    }
+
+    return (
+      <div className="flex flex-col gap-(--m)">  
+          <h1 className="text-[length:var(--texto-g)]">{ !id || !transacao ? 'Criar nova transação' : 'Editar transação'}</h1>
+          <Cartao classeAdicional="w-full">
+            <FormTransacoes transacao={transacao} />
+          </Cartao>
+      </div>
+    );
+}
---
+++ src/components/form-transacoes/form-transacoes.tsx
@@ -0,0 +1,130 @@
+'use client';
+import { ITransacao } from "@/interfaces/itransacao";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function FormTransacoes({ transacao }: { transacao: ITransacao | undefined }) {
+    const router = useRouter();
+
+    const cancelar = () => {
+      router.push('/inicio');
+    };
+    
+    const [formData, setFormData] = useState({
+      tipo: transacao?.tipo || 'Receita',
+      metodo: transacao?.metodo || 'Dinheiro',
+      descricao: transacao?.descricao || '',
+      valor: transacao?.valor.toFixed(2) || 0.00,
+      data: transacao?.data || new Date().toISOString().split('T')[0]
+    });
+
+    const handleChange = (e:any) => {
+      const { name, value } = e.target;
+      setFormData(prev => ({
+        ...prev,
+        [name]: value
+      }));
+    };
+
+    const handleSubmit = async (e:any) => {
+      e.preventDefault();
+      
+      console.log('Dados do formulário:', formData);
+      
+      // // Enviar dados para uma API route do Next.js
+      // const response = await fetch('/api/contact', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(formData),
+      // });
+
+      // if (response.ok) {
+      //   alert('Mensagem enviada com sucesso!');
+      //   setFormData({ tipo: '', email: '', message: '' });
+      // } else {
+      //   alert('Ocorreu um erro ao enviar a mensagem.');
+      // }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="w-full flex items-center justify-center py-(--m)">
+            <div className="max-w-[500px] flex flex-col gap-(--gg) w-full">
+                <div className="flex flex-col gap-(--m)">
+                    <div className="flex flex-col gap-(--p)">
+                        <label className="text-[length:var(--texto-p)]" htmlFor="tipo">Tipo</label>
+                        <select 
+                            className="input-select"
+                            name="tipo" 
+                            id="tipo"
+                            value={formData.tipo}
+                            onChange={handleChange}
+                            required
+                        >
+                            <option value="Receita">Receita</option>
+                            <option value="Despesa">Despesa</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col gap-(--p)">
+                        <label className="text-[length:var(--texto-p)]" htmlFor="metodo">Método</label>
+                        <select 
+                            className="input-select"
+                            name="metodo" 
+                            id="metodo"
+                            value={formData.metodo}
+                            onChange={handleChange}
+                            required
+                        >
+                            <option value="Dinheiro">Dinheiro</option>
+                            <option value="Transferência">Transferência</option>
+                            <option value="Depósito">Depósito</option>
+                            <option value="Cartão de crédito">Cartão de crédito</option>
+                            <option value="Cartão de débito">Cartão de débito</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col gap-(--p)">
+                        <label className="text-[length:var(--texto-p)]" htmlFor="descricao">Descrição</label>
+                        <input 
+                            className="input-text"
+                            type="text" 
+                            name="descricao"
+                            id="descricao"
+                            value={formData.descricao}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col gap-(--p)">
+                        <label className="text-[length:var(--texto-p)]" htmlFor="valor">Valor R$</label>
+                        <input 
+                            className="input-number"
+                            type="number" 
+                            name="valor"
+                            id="valor"
+                            value={formData.valor}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col gap-(--p)">
+                        <label className="text-[length:var(--texto-p)]" htmlFor="data">Data</label>
+                        <input 
+                            className="input-data"
+                            type="date" 
+                            name="data" 
+                            id="data" 
+                            value={formData.data}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center justify-center gap-(--m)">
+                    <button type="button" onClick={cancelar} className="botao-secundario">Cancelar</button>
+                    <button type="submit" className="botao-primario">Salvar</button>
+                </div>
+            </div>
+        </form>
+    );
+}
